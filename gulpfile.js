36c4cd456a38a65e799f2b1c285ec1a9003aba30
libertyernie <maybeway36@gmail.com>
2019-04-09 20:13:40 -0500

    Update pdf.js to version 2.1.266; fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -802,7 +802,7 @@ function preprocessDefaultPreferences(content) {
           content + '\n');
 }
 
-gulp.task('seamonkey-pre', ['buildnumber', 'locale'], function () {
+gulp.task('seamonkey-pre', gulp.series('buildnumber', 'locale', function () {
   console.log();
   console.log('### Building SeaMonkey extension');
   var defines = builder.merge(DEFINES, { FIREFOX: true, SKIP_BABEL: true, });
@@ -878,9 +878,9 @@ gulp.task('seamonkey-pre', ['buildnumber', 'locale'], function () {
     preprocessJS(FIREFOX_EXTENSION_DIR + 'bootstrap.js', defines, true)
         .pipe(gulp.dest(SEAMONKEY_BUILD_DIR)),
   ]);
-});
+}));
 
-gulp.task('seamonkey', ['seamonkey-pre'], function (done) {
+gulp.task('seamonkey', gulp.series('seamonkey-pre', function (done) {
   var FIREFOX_EXTENSION_FILES =
         ['bootstrap.js',
          'install.rdf',
@@ -908,7 +908,7 @@ gulp.task('seamonkey', ['seamonkey-pre'], function (done) {
     console.log('extension created: ' + FIREFOX_EXTENSION_NAME);
     done();
   });
-});
+}));
 
 gulp.task('mozcentral-pre', gulp.series('buildnumber', 'locale', function () {
   console.log();
